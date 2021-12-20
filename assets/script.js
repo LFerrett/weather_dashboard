5a8fc09611eed128c041f7bf5674920161a00e81
“Lauren <lferrett@gmail.com>
2021-12-20 11:16:43 -0500

    Commit of full pseudo code walk through of JS.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
+// Variables for api key, search, search button, city list, containers for current chosen city, forecast, five day forecast, and empty cities array to track search history
+var cities = [];
 var APIKey = "8756b0c1a73cc214da1eeb5ee10ebe91";
+
 var searchBtn = document.querySelector("#submit");
 var citySelected = document.querySelector('#searchTerm').value;
 var searchURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySelected + "&appid=" + APIKey;
@@ -8,6 +11,72 @@ var containerEl2 = document.querySelector('#container2');
 var containerEl3 = document.querySelector('#container3');
 var containerEl4 = document.querySelector('#container4');
 var containerEl5 = document.querySelector('#container5');
+
+// Create listener to listen for click on the search button
+// Check to see if the value of the search button is empty, if not, start the action
+searchBtn.click(function (event) {
+  if (search.val() !== "") {
+   // create massive function that captures all the actions needed
+    pullWeather();
+  } else {
+    alert("please enter a city");
+  }
+});
+
+// try to add a way for the enter button to initiate search
+// maybe find what the enter key code is and strictly look for that 
+// do the same actions as the search button
+
+
+// the meat and potatoes... throw all functions into this big one
+// things to do... unhide hidden search history and weather container
+// grab the search term inputted and push it into the cities array
+// capture local storage and create the cities list search history
+// pull the weather from the API
+// clear the search input once it's captured
+function pullWeather() {
+  event.preventDefault();
+// add more as you write and decide on variables
+// set local storage - strigifies cities for search history
+localStorage.setItem("cities", JSON.stringify(cities));
+ // add
+}
+
+// Make the cities list, use buttons for style and then add click event 
+// get weather if a city from history is clicked
+function makeCities() {
+  // add
+}
+
+// Connect and fetch OpenWeatherMap API (may have to do this multiple times for selected city, UV Index, then 5 day forecast)
+// using a function to fetch openweathermap api to get current weather conditions
+function getWeather(city) {
+  // Make the URL a variable that includes both the user inputted city and the API key
+  // fetch the URL, check for response
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+ // Display the current weather conditions for the selected city
+ var currentCity = $("#currentCity");
+ // use selected city conditions and use moment.js to append the curry date to the current city
+// set icons based on weather conditions 
+// set current icon and add to icon class
+// current icon is equal to the <img> tag and the conditions list weather icon
+// set the attribute of the icon to the src file and use a dynamically created URL to append to the current city/icon
+
+// store and display current temperature, humidity, wind speed, UV index (may need to do more for UV index)
+
+}
+
+// Figure out how to pull forecast weather, making a loop that stops when its pulled five days (OpenWeatherMap has seven day forecast)
+function createForecast(forecast) {
+// Things to remember: clear previous text before looping
+  fiveDayForecast.text("");
+  // loop - for (var i = 1; i < 6; i++)
+
+  var dailyForecast = forecast.daily;
+  // looping through forecasted days from api and stopping at the 5th day
+  // capture date, high temp (max) and low temp (min), humidity, and use the same icon string as above
+}
 
 function pullWeather() {
     console.log("citySelected");
@@ -37,42 +106,6 @@ function pullWeather() {
   //  }
 //})
 }
-
-var displayWeather = function (weather, citySelected) {
-    if (repos.length === 0) {
-      repoContainerEl.textContent = 'No repositories found.';
-      return;
-    }
-  
-    repoSearchTerm.textContent = searchTerm;
-  
-    for (var i = 0; i < repos.length; i++) {
-      var repoName = repos[i].owner.login + '/' + repos[i].name;
-  
-      var repoEl = document.createElement('a');
-      repoEl.classList = 'list-item flex-row justify-space-between align-center';
-      repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-  
-      var titleEl = document.createElement('span');
-      titleEl.textContent = repoName;
-  
-      repoEl.appendChild(titleEl);
-  
-      var statusEl = document.createElement('span');
-      statusEl.classList = 'flex-row align-center';
-  
-      if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-      } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-      }
-  
-      repoEl.appendChild(statusEl);
-  
-      repoContainerEl.appendChild(repoEl);
-    }
-  };
 
 
 searchBtn.addEventListener("click", pullWeather);
