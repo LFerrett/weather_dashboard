ce5a9bc3ec3e85b19fbd8bf9ecb04a2f36f0face
“Lauren <lferrett@gmail.com>
2021-12-20 22:34:58 -0500

    Working JS and some tweaks to css for styling purposes.

--- assets/script.js
+++ assets/script.js
@@ -1,138 +1,165 @@
-// Variables for api key, search, search button, city list, containers for current chosen city, forecast, five day forecast, and empty cities array to track search history
-var cities = [];
 var APIKey = "8756b0c1a73cc214da1eeb5ee10ebe91";
-
+var cities = [];
 var search = $(".search");
-var searchBtn = $(".searchBtn");
-var cityBtn;
+var searchBTN = $(".searchBTN");
+var historyBTN;
 var citiesList = $(".cities");
 var weatherContainer = $("#weatherContainer");
 var forecastContainer = $("#forecastContainer");
 var fiveDayForecast = $("#forecast");
 
-var cities = [];
+searchBTN.click(function (event) {
+  if (search.val() !== "") {
+    executeWeather();
+  } else {
+    alert("Enter a city to see the weather!");
+  }
+});
 
-// Create listener to listen for click on the search button
-// Check to see if the value of the search button is empty, if not, start the action
-searchBtn.click(function (event) {
+$(search).keypress(function (event) {
+  var keyCode = event.keyCode;
+  if (keyCode === 13) {
     if (search.val() !== "") {
-      executeFunctions();
+      executeWeather();
     } else {
       alert("Enter a city to see the weather!");
     }
+  }
+});
+
+function executeWeather() {
+  // event.preventDefault();
+  citiesList.removeClass("hidden");
+  weatherContainer.removeClass("hidden");
+  cities.push(search.val());
+  setStorage();
+  createCityList(search.val());
+  getWeather(search.val());
+  search.val("");
+}
+
+function createCityList(lastCity) {
+  searchBTN = $("<button>").addClass(
+    "searchBTN BTN BTN-outline-secondary d-flex w-100"
+  );
+  searchBTN.text(lastCity);
+  searchBTN.attr("data-city", lastCity);
+  citiesList.prepend(searchBTN);
+
+  searchBTN.click(function (event) {
+    getWeather(event.target.getAttribute("data-city"));
   });
-
-// try to add a way for the enter button to initiate search
-// maybe find what the enter key code is and strictly look for that 
-// do the same actions as the search button
-
-
-// the meat and potatoes... throw all functions into this big one
-// things to do... unhide hidden search history and weather container
-// grab the search term inputted and push it into the cities array
-// capture local storage and create the cities list search history
-// pull the weather from the API
-// clear the search input once it's captured
-function pullWeather() {
-  event.preventDefault();
-// add more as you write and decide on variables
-// set local storage - strigifies cities for search history
-localStorage.setItem("cities", JSON.stringify(cities));
- // add
 }
 
-// Make the cities list, use buttons for style and then add click event 
-// get weather if a city from history is clicked
-function makeCities() {
-  // add
+function setStorage() {
+  localStorage.setItem("cities", JSON.stringify(cities));
 }
 
-// Connect and fetch OpenWeatherMap API (may have to do this multiple times for selected city, UV Index, then 5 day forecast)
-// using a function to fetch openweathermap api to get current weather conditions
-  // Make the URL a variable that includes both the user inputted city and the API key
-  // fetch the URL, check for response
+function getWeather(city) {
+  var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
 
-  function getWeather(city) {
-    var APIKey = "8756b0c1a73cc214da1eeb5ee10ebe91";
-    var APIUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
-  
-    fetch(APIUrl).then(function (response) {
-      response.json().then(function (data) {
-        createCurrentConditions(data, city);
-      });
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      createCurrentConditions(data, city);
     });
-  }
- // Display the current weather conditions for the selected city
- // use selected city conditions and use moment.js to append the curry date to the current city
-// set icons based on weather conditions 
-// set current icon and add to icon class
-// current icon is equal to the <img> tag and the conditions list weather icon
-// set the attribute of the icon to the src file and use a dynamically created URL to append to the current city/icon
-// store and display current temperature, humidity, wind speed, UV index (may need to do more for UV index)
+  });
 }
-function pullUVI(lat, lon) {
-    var APIKey = "8756b0c1a73cc214da1eeb5ee10ebe91";
-    var uviUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
-  
-    fetch(uviUrl).then(function (response) {
-      response.json().then(function (data) {
-        createCurrentUvi(data);
-      });
-    });
-  }
-  
-  function getForecast(lat, lon) {
-    var APIKey = "8756b0c1a73cc214da1eeb5ee10ebe91";
-    var forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,current,alerts&units=imperial&appid=${APIKey}`;
-  
-    fetch(forecastUrl).then(function (response) {
-      response.json().then(function (data) {
-        createForecast(data);
-      });
-    });
-  }
-  
-  function setStorage() {
-  }
 
-// Figure out how to pull forecast weather, making a loop that stops when its pulled five days (OpenWeatherMap has seven day forecast)
+function getUvi(lat, lon) {
+  var uviUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
+
+  fetch(uviUrl).then(function (response) {
+    response.json().then(function (data) {
+      createCurrentUvi(data);
+    });
+  });
+}
+
+function getForecast(lat, lon) {
+  var forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,current,alerts&units=imperial&appid=${APIKey}`;
+
+  fetch(forecastUrl).then(function (response) {
+    response.json().then(function (data) {
+      createForecast(data);
+    });
+  });
+}
+
+function createCurrentConditions(conditions, city) {
+  var currentCity = $("#currentCity");
+  currentCity.text(conditions.city.name);
+
+  var currentDate = $("<span>");
+  currentDate.text(` (${moment(conditions.list.dt_txt).format("MMMM Do")})`);
+  currentCity.append(currentDate);
+
+  var currentIcon = $("<img>");
+  var icon = conditions.list[0].weather[0].icon;
+  var iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  currentIcon.attr("src", iconUrl);
+  currentCity.append(currentIcon);
+
+  var currentTemp = $("#currentTemp");
+  currentTemp.text(conditions.list[0].main.temp);
+
+  var currentHumidity = $("#currentHumidity");
+  currentHumidity.text(conditions.list[0].main.humidity);
+
+  var currentWind = $("#currentWind");
+  currentWind.text(conditions.list[0].wind.speed);
+
+  var lat = conditions.city.coord.lat;
+  var lon = conditions.city.coord.lon;
+  getUvi(lat, lon);
+  getForecast(lat, lon);
+}
+
+function createCurrentUvi(uvi) {
+  var currentUVI = $("#currentUVI").addClass("text-white p-1");
+  var uviIndex = uvi.current.uvi;
+  currentUVI.text(uviIndex);
+
+  if (uviIndex <= 2) {
+    currentUVI.addClass("bg-success");
+  } else if (uviIndex > 2 && uviIndex <= 7) {
+    currentUVI.addClass("bg-warning");
+  } else if (uviIndex > 7) {
+    currentUVI.addClass("bg-danger");
+  }
+}
+
 function createForecast(forecast) {
-// Things to remember: clear previous text before looping
   fiveDayForecast.text("");
-  // loop - for (var i = 1; i < 6; i++)
 
   var dailyForecast = forecast.daily;
-  // looping through forecasted days from api and stopping at the 5th day
-  // capture date, high temp (max) and low temp (min), humidity, and use the same icon string as above
+  for (var i = 1; i < 6; i++) {
+    var nextDay = dailyForecast[i];
+    var forcastedDay = moment(nextDay.dt * 1000).format("MMMM Do");
+
+    var dayWeather = $("<div>").addClass(
+      "card purple d-flex text-white p-3 col-8 col-xl-2 col-md-4 col-sm-4 m-3"
+    );
+    fiveDayForecast.append(dayWeather);
+
+    var weatherDate = $("<h5>").addClass("d-flex justify-content-center");
+    weatherDate.text(forcastedDay);
+    dayWeather.append(weatherDate);
+
+    var forecastIcon = nextDay.weather[0].icon;
+    var forecastIconUrl = `http://openweathermap.org/img/wn/${forecastIcon}@2x.png`;
+    var weatherIcon = $("<img>").attr("src", forecastIconUrl);
+    dayWeather.append(weatherIcon);
+
+    var maxTemp = $("<span>").addClass("text-start");
+    maxTemp.text(`Max: ${nextDay.temp.max}°F`);
+    dayWeather.append(maxTemp);
+
+    var minTemp = $("<span>").addClass("text-start");
+    minTemp.text(`Min: ${nextDay.temp.min}°F`);
+    dayWeather.append(minTemp);
+
+    var dayHumidity = $("<span>").addClass("text-start");
+    dayHumidity.text(`Humidity: ${nextDay.humidity}%`);
+    dayWeather.append(dayHumidity);
+  }
 }
-
-function pullWeather() {
-    console.log("citySelected");
-
-   // fetch(searchURL)
-        // The browser fetches the resource from the remote server without first looking in the cache.
-        // The browser will then update the cache with the downloaded resource.
-    //    cache: 'reload',
-      
-    //    then(function (response) {
-    //      return response.json();
-    //    })
-    //    then(function (data) {
-   //       console.log(data);
-    //    });
-      
-
-
-
-
-    //fetch(searchURL.then(function (response) {
-   // if (response.ok) {
-     // response.json().then(function (data) {
-       //displayWeather(data.items, language);
-      // console.log("Contact Made");
-    // })
-  //  }
-//})
-}
-
-searchBtn.addEventListener("click", executeFunctions);
